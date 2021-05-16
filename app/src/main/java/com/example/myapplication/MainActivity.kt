6592b3ca38d0e6549bda20e516056c850f69e19d
JOEY <joey60504@gmail.com>
2021-05-16 17:05:55 +0800

    首次上傳

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.myapplication
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+    fun signin(p0: View){
+        startActivity(Intent(this,signin::class.java))
+    }
+}
