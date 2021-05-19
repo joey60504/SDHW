fb8353b4731313b3b4eb6339c2aa4d851ff791cc
JOEY <joey60504@gmail.com>
2021-05-19 16:21:32 +0800

    可運作半成品

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,24 +1,90 @@
 package com.example.myapplication
 
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.EditText
-import android.widget.TextView
 import android.widget.Toast
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.IdpResponse
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlin.math.sign
 
 class MainActivity : AppCompatActivity() {
+    val RC_SIGN_IN=303
+    private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-  //      findViewById<TextView>(R.id.textView2).setOnClickListener {
-  //        startActivity(Intent(this,signin::class.java)) }
-    }
-    fun Singin(p0: View){
-        startActivity(Intent(this,signin::class.java))
+        // Initialize Firebase Auth
+        auth = Firebase.auth
+
     }
 
+    fun login(){
+        // Choose authentication providers
+        val providers = arrayListOf(
+            AuthUI.IdpConfig.EmailBuilder().build(),
+            AuthUI.IdpConfig.GoogleBuilder().build()
+        )
+
+        // Create and launch sign-in intent
+        startActivityForResult(
+            AuthUI.getInstance()
+                .createSignInIntentBuilder()
+                .setAvailableProviders(providers)
+                .setLogo(R.drawable.male)
+                .build(),
+            RC_SIGN_IN)
+    }
+
+    public override fun onStart() {
+        super.onStart()
+        // Check if user is signed in (non-null) and update UI accordingly.
+        val currentUser = auth.currentUser
+        if(currentUser != null){
+            Log.d("DEBUG","currentUser=${currentUser.email}")
+            Toast.makeText(this,"Welcome ${currentUser.email}",Toast.LENGTH_SHORT).show()
+        }else{
+            login()
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == RC_SIGN_IN) {
+            val response = IdpResponse.fromResultIntent(data)
+
+            if (resultCode == Activity.RESULT_OK) {
+                // Successfully signed in
+                val user = FirebaseAuth.getInstance().currentUser
+                Log.d("DEBUG","res=${response.toString()}")
+        //        Toast.makeText(this,"Welcome ${user.email}",Toast.LENGTH_LONG).show()
+                // ...
+            } else {
+                // Sign in failed. If response is null the user canceled the
+                // sign-in flow using the back button. Otherwise check
+                // response.getError().getErrorCode() and handle the error.
+                // ...
+            }
+        }
+    }
+    fun logout(p0: View){
+        AuthUI.getInstance()
+            .signOut(this)
+            .addOnCompleteListener {
+                // ...
+                Toast.makeText(this,"You logout now",Toast.LENGTH_SHORT).show()
+                recreate()
+            }
+    }
+    fun signin(p0: View){
+        startActivity(Intent(this,signin::class.java))
+    }
 }
