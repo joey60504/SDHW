7ebb66304b64346cc211d94034ebe0e9c24c09ec
JOEY <joey60504@gmail.com>
2021-05-19 13:40:21 +0800

    修改

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -5,15 +5,20 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.math.sign
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+  //      findViewById<TextView>(R.id.textView2).setOnClickListener {
+  //        startActivity(Intent(this,signin::class.java)) }
     }
-    fun signin(p0: View){
+    fun Singin(p0: View){
         startActivity(Intent(this,signin::class.java))
     }
+
 }
