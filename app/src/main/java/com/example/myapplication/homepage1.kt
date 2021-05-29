9998be804f2953583f9bf287b7f6d7c24035757b
mikko <mikko88101218@gmail.com>
2021-05-29 16:02:46 +0800

    錯誤

--- app/src/main/java/com/example/myapplication/homepage1.kt
+++ app/src/main/java/com/example/myapplication/homepage1.kt
@@ -2,10 +2,17 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import kotlinx.android.synthetic.main.activity_test1.*
 
 class homepage1 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_homepage1)
+        val datatest= listOf("data1","data2","data3")
+        val adapter= ArrayAdapter(this,android.R.layout.simple_spinner_dropdown_item,datatest)
+        findViewById<Spinner>(R.id.spinner).adapter=adapter
     }
+
 }
