742a05de22f4e4c38e9ef1da15615547031ea464
leo666661@gmail.com <Leo2046988321>
2021-07-06 00:05:47 +0800

    v2.0 頁面待接在電話驗證認證後一頁

--- app/src/main/java/com/example/myapplication/RegistrationActivity1.kt
+++ app/src/main/java/com/example/myapplication/RegistrationActivity1.kt
@@ -6,13 +6,14 @@ import android.text.TextUtils
 import android.widget.Toast
 import com.example.myapplication.R
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.PhoneAuthCredential
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
 import kotlinx.android.synthetic.main.activity_registration1.*
 
 class RegistrationActivity : AppCompatActivity() {
     lateinit var auth: FirebaseAuth
-    var databaseReference :  DatabaseReference? = null
+    var databaseReference: DatabaseReference? = null
     var database: FirebaseDatabase? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -31,38 +32,42 @@ class RegistrationActivity : AppCompatActivity() {
 
         registerButton.setOnClickListener {
 
-            if(TextUtils.isEmpty(firstnameInput.text.toString())) {
+            if (TextUtils.isEmpty(firstnameInput.text.toString())) {
                 firstnameInput.setError("請輸入名稱 ")
                 return@setOnClickListener
-            } else if(TextUtils.isEmpty(lastnameInput.text.toString())) {
+            } else if (TextUtils.isEmpty(lastnameInput.text.toString())) {
                 firstnameInput.setError("請輸入年齡")
                 return@setOnClickListener
-            }else if(TextUtils.isEmpty(usernameInput.text.toString())) {
+            } else if (TextUtils.isEmpty(usernameInput.text.toString())) {
                 firstnameInput.setError("請輸入生理性別")
                 return@setOnClickListener
-            }else if(TextUtils.isEmpty(passwordInput.text.toString())) {
+            } else if (TextUtils.isEmpty(passwordInput.text.toString())) {
                 firstnameInput.setError("請輸入駕照照片URL")
                 return@setOnClickListener
             }
 
-
-            auth.signInWithCredential(credential)
-                .addOnCompleteListener(this) {
-                    if(task.isSuccessful) {
-                        val currentUser = auth.currentUser
-                        val currentUSerDb = databaseReference?.child((currentUser?.uid!!))
-                        currentUSerDb?.child("名稱")?.setValue(firstnameInput.text.toString())
-                        currentUSerDb?.child("年齡")?.setValue(lastnameInput.text.toString())
-                        currentUSerDb?.child("性別")?.setValue(usernameInput.text.toString())
-                        currentUSerDb?.child("駕照照片")?.setValue(passwordInput.text.toString())
-
-                        Toast.makeText(this@RegistrationActivity, "登錄成功", Toast.LENGTH_LONG).show()
-                        finish()
-
-                    } else {
-                        Toast.makeText(this@RegistrationActivity, "登錄失敗 ", Toast.LENGTH_LONG).show()
-                    }
-                }
         }
+    }
+
+    private fun signInWithPhoneAuthCredential(credential: PhoneAuthCredential) {
+        auth.signInWithCredential(credential)
+            .addOnCompleteListener(this) {
+                if (it.isSuccessful) {
+                    val currentUser = auth.currentUser
+                    val currentUSerDb = databaseReference?.child((currentUser?.uid!!))
+                    currentUSerDb?.child("名稱")?.setValue(firstnameInput.text.toString())
+                    currentUSerDb?.child("年齡")?.setValue(lastnameInput.text.toString())
+                    currentUSerDb?.child("性別")?.setValue(usernameInput.text.toString())
+                    currentUSerDb?.child("駕照照片")?.setValue(passwordInput.text.toString())
+
+                    Toast.makeText(this@RegistrationActivity, "登錄成功", Toast.LENGTH_LONG)
+                        .show()
+                    finish()
+
+                } else {
+                    Toast.makeText(this@RegistrationActivity, "登錄失敗 ", Toast.LENGTH_LONG)
+                        .show()
+                }
+            }
     }
 }
