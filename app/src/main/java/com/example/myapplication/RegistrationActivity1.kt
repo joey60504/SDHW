5aabf33fef9f67e702c505195689ce32bb08b653
leo666661@gmail.com <Leo2046988321>
2021-07-16 00:04:55 +0800

    資料庫profile子類別架設ok

--- app/src/main/java/com/example/myapplication/RegistrationActivity1.kt
+++ app/src/main/java/com/example/myapplication/RegistrationActivity1.kt
@@ -22,6 +22,7 @@ class RegistrationActivity1 : AppCompatActivity() {
         setContentView(R.layout.activity_registration1)
 
         auth = FirebaseAuth.getInstance()
+        var phone=auth.currentUser?.phoneNumber.toString()
 
         val registerButton : Button = findViewById(R.id.registerButton)
 
@@ -39,7 +40,7 @@ class RegistrationActivity1 : AppCompatActivity() {
 
             if (name.isNotEmpty() && email.isNotEmpty() && age.isNotEmpty() && gender.isNotEmpty() && photo.isNotEmpty()) {
                 val Users=User(name, email, age, gender, photo)
-                database.child(name).setValue(Users)
+                database.child("proflie").child(phone).setValue(Users)
                     .addOnCompleteListener {
                         Toast.makeText(this, "註冊成功", Toast.LENGTH_SHORT).show()
                         startActivity(Intent(this, choice::class.java))
