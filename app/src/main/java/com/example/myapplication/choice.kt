1e304cf8e212487a1a57c9b5e4d7dd8ae7da4261
JOEY <joey60504@gmail.com>
2021-07-19 04:54:01 +0800

    個人資料統整 刪除profilepage 將內容新增至漢堡選單內

--- app/src/main/java/com/example/myapplication/choice.kt
+++ app/src/main/java/com/example/myapplication/choice.kt
@@ -5,7 +5,13 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import kotlinx.android.synthetic.main.fragment_personinformation.*
 
 class choice : AppCompatActivity() {
 
@@ -14,30 +20,27 @@ class choice : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_choice)
-
+//登入
         auth= FirebaseAuth.getInstance()
         var currentUser=auth.currentUser
-
-//        Reference
         val logout=findViewById<Button>(R.id.idLogout)
-
         if(currentUser==null){
             startActivity(Intent(this,MainActivity::class.java))
             finish()
         }
-
         logout.setOnClickListener{
             auth.signOut()
             startActivity(Intent(this,MainActivity::class.java))
             finish()
         }
-
+//完
+//換頁
     }
     fun commute(p0: View){
         startActivity(Intent(this,homepage::class.java))
     }
     fun test(p0: View){
-        startActivity(Intent(this,ProfilePage::class.java))
+        startActivity(Intent(this,ProfileActivity::class.java))
     }
-
+//換頁完
 }
--- app/src/main/java/com/example/myapplication/ui/personinformation/personinformation.kt
+++ app/src/main/java/com/example/myapplication/ui/personinformation/personinformation.kt
@@ -7,15 +7,19 @@ import android.view.View
 import android.view.ViewGroup
 import com.example.myapplication.databinding.FragmentNavTestBinding
 import com.example.myapplication.databinding.FragmentPersoninformationBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import kotlinx.android.synthetic.main.fragment_personinformation.*
 
 
 private var _binding: FragmentPersoninformationBinding?=null
 private val binding get() = _binding!!
 
 class personinformation : Fragment() {
-    private var param1: String? = null
-    private var param2: String? = null
-
+    lateinit var auth: FirebaseAuth
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -23,6 +27,34 @@ class personinformation : Fragment() {
     ): View? {
         _binding = FragmentPersoninformationBinding.inflate(inflater, container, false)
         val root: View = binding.root
+//資料庫
+        auth = FirebaseAuth.getInstance()
+        var phone = auth.currentUser?.phoneNumber.toString()
+        var database = FirebaseDatabase.getInstance().reference
+        var getdata=object : ValueEventListener {
+            override fun onCancelled(error: DatabaseError) {
+
+            }
+
+            override fun onDataChange(p0: DataSnapshot) {
+                for (i in p0.children) {
+                    var name=i.child(phone).child("name").getValue()
+                    var age=i.child(phone).child("age").getValue()
+                    var gender=i.child(phone).child("gender").getValue()
+                    var photo=i.child(phone).child("photo").getValue()
+                    var email=i.child(phone).child("email").getValue()
+                    textView64.text=name.toString()
+                    textView70.text=age.toString()
+                    textView71.text=gender.toString()
+                    textView72.text=photo.toString()
+                    textView69.text=email.toString()
+                    textView74.text=phone
+                }
+            }
+        }
+        database.addValueEventListener(getdata)
+//資料庫完
         return root
     }
+
 }
