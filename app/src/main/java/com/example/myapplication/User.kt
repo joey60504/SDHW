d691461ad3308d8bedfccc339023b0d10617180a
小黃 <william2000123@gmail.com>
2021-07-22 00:07:36 +0800

    後端交接用勿載

--- app/src/main/java/com/example/myapplication/User.kt
+++ app/src/main/java/com/example/myapplication/User.kt
@@ -2,5 +2,5 @@ package com.example.myapplication
 
 class User(val name : String="",val email : String="" ,val age : String="",val gender : String="",val photo : String=""){
 
-
+    constructor() : this("", "", "", "","")
 }
