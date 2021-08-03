23e96df5cba742d4137fa5a6ce1fb1492288acf5
小黃 <william2000123@gmail.com>
2021-08-04 00:00:16 +0800

    小花專用

---
+++ app/src/main/java/com/example/myapplication/chatmessage.kt
@@ -0,0 +1,6 @@
+package com.example.myapplication
+
+class chatmessage (val id: String,val text: String,val fromid: String,val toid: String,
+    val timestamp: Long){
+        constructor() : this("", "", "", "", -1)
+}
